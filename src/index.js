08d902746e92231e07ad33556072bf48bb4e234c
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2023-09-07 12:44:47 -0700

    Added css baseline

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
 import App from "./App";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
